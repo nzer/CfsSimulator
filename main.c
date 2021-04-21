7e9bb4c9af2785aeb543dca336387b65f2ae6060
Alim <alim.naizabek@gmail.com>
2021-04-21 19:55:10 +0600

    finally fix debug

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "rbt.h"
 
+#define TIMER 10
+
 int main(int argc, char const *argv[])
 {
     tree_t tree;
     tree.root = NULL;
 
     // Input format: name pri burst appearance
-    FILE* input = fopen("/home/alim/Projects/CfsSimulator/input.txt", "r");
+    FILE* input = fopen("input.txt", "r");
     int input_size = 0;
     fscanf(input, "%d", &input_size);
     for (size_t i = 0; i < input_size; i++)
@@ -17,5 +19,10 @@ int main(int argc, char const *argv[])
         insert(&tree, task);
     }
     fclose(input);
+    while (tree.root != NULL)
+    {
+        
+    }
+    
     return 0;
 }
